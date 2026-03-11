a40e855d8c7088b490689881574853b5e6366424
mdmokhlesur <mokhlesurinfo5@gmail.com>
2026-03-11 11:40:05 +0600

    feat: create SidebarHeader component to conditionally display a logo or summary overview based on page context.

--- components/SidebarHeader.tsx
+++ components/SidebarHeader.tsx
@@ -60,10 +60,10 @@ const SidebarHeader = ({
       </div>
 
       <svg xmlns="http://www.w3.org/2000/svg" width="300" height="2" viewBox="0 0 300 2" fill="none">
-        <path d="M0 0.750183H299.315" stroke="#1F2F3D" stroke-width="1.50039" stroke-miterlimit="10" />
+        <path d="M0 0.750183H299.315" stroke="#1F2F3D" strokeWidth="1.50039" strokeMiterlimit="10" />
       </svg>
       <svg xmlns="http://www.w3.org/2000/svg" width="300" height="2" viewBox="0 0 300 2" fill="none">
-        <path d="M0 0.750183H299.315" stroke="#456987" stroke-width="1.50039" stroke-miterlimit="10" />
+        <path d="M0 0.750183H299.315" stroke="#456987" strokeWidth="1.50039" strokeMiterlimit="10" />
       </svg>
     </div>
   );
